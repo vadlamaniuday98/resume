cd9623e05bee56e62fd0d5f485d39fc2da4ef19a
Vadlamani Uday <vadlamaniuday04@gmail.com>
2024-07-11 17:41:35 +0530

    added speed insights from vercel

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
 
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
